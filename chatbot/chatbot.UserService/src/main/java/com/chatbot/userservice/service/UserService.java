1e803967ee718c34ad271955c38b590e121814b4
AngelMercado <myjava@outlook.es>
2018-10-08 21:18:51 -0500

    Chore: removed trash

--- chatbot/chatbot.UserService/src/main/java/com/chatbot/userservice/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.chatbot.userservice.service;
-
-import com.chatbot.userservice.model.User;
-
-
-public interface UserService {
-	User createUser(User user);
-	User getUserByUsername(String username);
-}
--- chatbot/chatbot.authenticationserver/src/main/java/com/itemsharing/authenticationserver/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.itemsharing.authenticationserver.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.itemsharing.authenticationserver.model.User;
-
-public interface UserRepository extends CrudRepository<User,Long>{
-	User findByUsername(String username);
-}
